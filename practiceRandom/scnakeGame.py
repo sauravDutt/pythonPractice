3a2bbb89372da1a2796b652a2b013e9212e4196e
sauravDutt <sauravd069@gmail.com>
2022-01-17 21:56:14 +0530

    Just added some more stuff

---
+++ practiceRandom/scnakeGame.py
@@ -0,0 +1,14 @@
+from turtle import left, right
+import pygame
+import sys
+import random
+
+class Snake():
+    def __init__(self):
+        self.length = 1
+        self.positions = [((screen_width/2), (screen_height/2))]
+        self.direction = random.choice([up, down, left, right])
+        self.color = (17, 24, 47)
+        self.score = 0
+    
+    
