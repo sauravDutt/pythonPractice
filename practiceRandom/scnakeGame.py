f3596f0e024c7071f53189e8d07d3af75128bc86
sauravDutt <sauravd069@gmail.com>
2022-01-17 22:13:24 +0530

    Just added some more stuff

--- practiceRandom/scnakeGame.py
+++ practiceRandom/scnakeGame.py
@@ -11,4 +11,24 @@ class Snake():
         self.color = (17, 24, 47)
         self.score = 0
     
+    def get_head_postion(self):
+        return self.positions[0]
     
+    def turn(self, point):
+        if self.length > 1 and (point[0]*-1, point[1]*-1) == self.direction:
+            return
+        else:
+            self.direction = point
+    
+    def move(self):
+        cur = self.get_head_postion()
+        x, y = self.direction
+        new = (((cur[0]+(x*gridsize)) %screen_width), (cur[0]+(y*gridsize)) %screen_height)
+        if len(self.positions) > 2 and new in self.positions[2: ]:
+            self.reset()
+        else:
+            self.positions.insert(0, new)
+            if len(self.positions) > self.length:
+                self.positions.pop()
+    
+    # def reset(self):
