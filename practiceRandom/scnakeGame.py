933b53f3a15956a154ac40db8d996ee679ee17ae
sauravDutt <sauravd069@gmail.com>
2022-01-18 23:00:38 +0530

    Just added some more stuff

--- practiceRandom/scnakeGame.py
+++ practiceRandom/scnakeGame.py
@@ -1,4 +1,3 @@
-from html import entities
 import pygame
 import sys
 import random
@@ -57,5 +56,42 @@ class Snake():
                     self.turn(left)
                 elif event.key == pygame.K_RIGHT:
                     self.turn(right)
+
+
+class Food():
+    def __init__(self):
+        self.position = (0, 0)
+        self.color = (223, 163, 49)
+        self.randomize_position()
+
+    def randomize_position():
+        self.position = (random.randint(0, grid_width-1)*gridsize, random.randint(0, grid_height-1)*gridsize)
+    
+    def draw(self, surface):
+        r = pygame.Rect((self.position[0], self.position[1]), (gridsize, gridsize))
+        pygame.draw.rect(surface, self.color, r)
+        pygame.draw.rect(surface, (93, 216, 228), r, 1)
+
+def drawGrid(surface):
+    for y in range(0, int(grid_height)):
+        for x in range(0, int(grid_width)):
+            if(x+y)%2 == 0:
+                r = pygame.Rect((x*gridsize, y*gridsize), (gridsize, gridsize))
+                pygame.draw.rect(surface, (93, 216, 228), r)
+            else:
+                rr = pygame.Rect((x*gridsize, y*gridsize), (gridsize, gridsize))
+                pygame.draw.rect(surface, (84, 194, 205), rr)
+
+screen_width = 480
+screen_height = 480
+
+gridsize = 20
+grid_width = screen_width/gridsize
+grid_height = screen_height/gridsize
+
+up = (0, -1)
+down = (0, 1)
+left = (-1, 0)
+right = (1, 0)
 
 
