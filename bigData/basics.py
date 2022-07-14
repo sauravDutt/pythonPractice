e4513f13a7e13314c27efd159f4862bbd4345897
sauravDutt <sauravd069@gmail.com>
2022-07-14 21:32:10 +0530

    Just started with it

---
+++ bigData/basics.py
@@ -0,0 +1,5 @@
+# Starting with Lists and tuples for our Data Analytics journey....
+# ------------------------------------------------------------------
+
+# This file is a Practice of Python for Data Analytics and a little bit of Automation.
+# The entire Repository is on Jupyter Notebook
